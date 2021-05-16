2fab76b00cd295344e59e387a8d61202d94fc0f7
Adwaith-Rajesh <adwaithrajesh3180@gmail.com>
2021-05-16 19:34:37 +0530

    Added COS_VENV_DIR to config | Updated requirements

--- cos/config.py
+++ cos/config.py
@@ -4,3 +4,6 @@ import os
 AUTHOR_NAME = os.environ.get("AUTHOR_NAME", "")
 AUTHOR_EMAIL = os.environ.get("AUTHOR_EMAIL", "")
 GITHUB_USER = os.environ.get("GITHUB_USER", "")
+
+# venv API
+COS_VENV_DIR = os.environ.get("COS_VENV_DIR", ".")
